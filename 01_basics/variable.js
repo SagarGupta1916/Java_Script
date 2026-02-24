9ec26f42993ec204581f559795d896fc18a192f9
SagarGupta1916 <sagargupta20211@gmail.com>
2026-02-24 09:41:52 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const accid = 12312
+let accEmail = "sagar@gmail.com"
+var accPass = "2525"
+accCity = "Delhi"
+let accState
+
+//accid = 2
+accEmail = "sg@gmail.com"
+accPass = "2021"
+accCity = "Mumbai"
+
+console.table([accid, accEmail, accPass, accCity, accState])
