e4fdcec2165a84313082e23589c5cc606a0c0cca
SagarGupta1916 <sagargupta20211@gmail.com>
2026-02-25 09:09:46 +0000

    operations

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -1,4 +1,4 @@
-let score = "33"
+// let score = "33"
 
 /*console.log(typeof score);
 let valueInNumber = Number(score)
@@ -9,7 +9,18 @@ console.log(valueInNumber)*/
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);*/
 
-let someNumber = 25
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 25
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//******************** Operations ***********************
+
+// let str1 = "Sagar"
+// let str2 = " Gupta"
+// let str3 = str1 + str2
+// console.log(str3)
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter)
