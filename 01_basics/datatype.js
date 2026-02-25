13ae73a6d4a1f77ac5fcc9d8a373c8f3f29f0f42
SagarGupta1916 <sagargupta20211@gmail.com>
2026-02-25 08:35:09 +0000

    config done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,14 @@
+"uss strict"; // treat all JS code as newer version
+
+//number 
+//bigint
+//string=>""
+//boolean=>true/false
+//null
+//uncefined
+//symbol
+
+//object
+//console.log(typeof"Sagar");
+//console.log(typeof null);
+console.log(typeof undefined);
