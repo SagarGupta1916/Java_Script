6f0a7fe398928f0cf87b3846f2b1d826052b7dda
SagarGupta1916 <sagargupta20211@gmail.com>
2026-04-17 12:43:52 +0000

    memoey type done

---
+++ 01_basics/memory.js
@@ -0,0 +1,16 @@
+let myname = "sagarguptaji"
+let anothername = myname
+anothername = "vk"
+console.log(anothername);
+console.log(myname);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sagar@goggle.com"
+console.log(userOne.email);
+console.log(userTwo.email);
