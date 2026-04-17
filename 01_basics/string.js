9d13bd1a43fb76d436454093f9e66abbd667951d
SagarGupta1916 <sagargupta20211@gmail.com>
2026-04-17 13:34:03 +0000

    string done

---
+++ 01_basics/string.js
@@ -0,0 +1,19 @@
+const name = 'sagar'
+const repoCount = 50
+
+//console.log(name + repoCount);
+
+/*console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);*/
+
+const Name = new String('sagar')
+console.log(Name);
+
+/*console.log(Name.length);
+console.log(Name[0]);
+console.log(Name.__proto__);*/
+
+//console.log(Name.charAt(3));
+//console.log(Name.indexOf('a'));
+
+const newString = Name.substring(0,3);
+console.log(newString);
